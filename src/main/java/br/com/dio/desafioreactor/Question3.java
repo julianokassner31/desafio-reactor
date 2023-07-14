c71a71d451e6399b65b5dd779ddddcf86fb674a2
Juliano Kassner <jkassner@lemontech.com.br>
2023-07-14 18:18:24 -0300

    Desafio reactor

--- src/main/java/br/com/dio/desafioreactor/Question3.java
+++ src/main/java/br/com/dio/desafioreactor/Question3.java
@@ -2,12 +2,19 @@ package br.com.dio.desafioreactor;
 
 import reactor.core.publisher.Mono;
 
+import java.util.function.Consumer;
+import java.util.function.Function;
+
 public class Question3 {
 
     /*
     Verifica se o usuário passado é valido, caso seja retorna void, caso contrário deve disparar uma exception
     (para esse desafio vamos considerar que o usário é valido quando ele tem uma senha com mais de 8 caractéres)
      */
-    public Mono<Void> userIsValid(final User user){}
-
+    public Mono<Void> userIsValid(final User user){
+        return Mono.justOrEmpty(user)
+                .filter(u -> u.password().length() > 8)
+                .switchIfEmpty(Mono.error(Exception::new))
+                .then();
+    }
 }
